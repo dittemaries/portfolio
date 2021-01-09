dd9373d7da5e3e72888837b0a15151d4b149f6e6
Ditte <ditt071b@stud.kea.dk>
2021-01-09 17:44:39 +0100

    tema 4 billeder og fonte, inkl. java element

---
+++ tema4.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", startScreen);
+
+function startScreen() {
+    console.log("startScreen");
+
+    document.querySelector("#container_egern").classList.add("bane");
+}
